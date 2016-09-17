a7506b414fe7579b62e3931129a232d60f9d7882
Mahesh Shivamallappa <mahesh.shivamallappa@rea-group.com>
2016-09-17 23:16:45 +1000

    check number of items on menu

---
+++ module1-assignment/app.js
@@ -0,0 +1,25 @@
+(function() {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+
+  $scope.checkNumberOfItems = function () {
+      var arrayOfItems = $scope.menu.split(',');
+      if (arrayOfItems.length <= 3) {
+        $scope.message = "Enjoy!";
+      }
+      else {
+        $scope.message = "Too much!"
+      }
+  };
+
+};
+
+
+})();
